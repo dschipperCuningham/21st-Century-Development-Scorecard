5da36c3b321087fe9f048db654f1052fde2b1765
Dustin Schipper <dschipper@cuningham.com>
2020-08-19 19:41:32 -0500

    initialization

---
+++ APP.js
@@ -0,0 +1,20 @@
+var matrixData = dataIn['matrix']
+var precedentData = dataIn['developments']
+
+var matrixId = 'matrixCanvas'
+var matrixCanvas = document.getElementById(matrixId)
+var matrixWidth = matrixCanvas.clientWidth
+var matrixHeight = matrixCanvas.clientHeight
+var interactiveMatrix = new DataEntryMatrix(matrixData,matrixCanvas,matrixWidth,matrixHeight);
+
+function resizeMatrix(){
+    console.log('the matrix was resized')
+    interactiveMatrix.resize(
+        document.getElementById(matrixId).clientWidth,
+        document.getElementById(matrixId).clientHeight
+    )
+}
+
+window.onresize = function(){
+    resizeMatrix()
+}
