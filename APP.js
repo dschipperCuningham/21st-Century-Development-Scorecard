57c5e341f704e22865e7195de24e1c6284cb1401
Dustin Schipper <dschipper@cuningham.com>
2020-08-19 21:04:22 -0500

    Stable simple matrix generation from source data

--- APP.js
+++ APP.js
@@ -6,9 +6,10 @@ var matrixCanvas = document.getElementById(matrixId)
 var matrixWidth = matrixCanvas.clientWidth
 var matrixHeight = matrixCanvas.clientHeight
 var interactiveMatrix = new DataEntryMatrix(matrixData,matrixCanvas,matrixWidth,matrixHeight);
+interactiveMatrix.drawUpdate()
 
 function resizeMatrix(){
-    console.log('the matrix was resized')
+    // console.log('the matrix was resized')
     interactiveMatrix.resize(
         document.getElementById(matrixId).clientWidth,
         document.getElementById(matrixId).clientHeight
--- Scripts/dataEntryMatrix.js
+++ Scripts/dataEntryMatrix.js
@@ -13,6 +13,7 @@ class DataEntryMatrix{
         this.centerY = _height/2;
         this.container = _div;
         this.data = this.buildChartData(_data);
+        console.log(this.data)
         //////////////////////////////////////////////////////////////////////////////////
         "///////ANY GRAPHIC SPECIFIC DATA SHAPING SHOULD HAPPEN HERE////////"
         //////////////////////////////////////////////////////////////////////////////////
@@ -24,17 +25,17 @@ class DataEntryMatrix{
         //////////////////////////////////////////////////////////////////////////////////
         "///////CREATING AN SVG OBJECT TO DRAW TO////////"
         //////////////////////////////////////////////////////////////////////////////////
-        var _svg = d3.select(_div).append("svg")
-            .attr("width",_width)
-            .attr("height",_height);
-        this.svg = _svg;
-        this.svgGroup = _svg.append("g").attr("class","svgGroup")
+        // var _svg = d3.select(_div).append("svg")
+        //     .attr("width",_width)
+        //     .attr("height",_height);
+        // this.svg = _svg;
+        // this.svgGroup = _svg.append("g").attr("class","svgGroup")
         //////////////////////////////////////////////////////////////////////////////////
         "///////ADD GROUPS TO THE SVG FOR ORGANIZING DATA////////"
         //////////////////////////////////////////////////////////////////////////////////
-        this.programNames = this.svgGroup.append("g").attr("class","programShapes");
-        this.intersectionCells = this.svgGroup.append("g").attr("class","programAnnotation");
-
+        // this.programNames = this.svgGroup.append("g").attr("class","programShapes");
+        // this.intersectionCells = this.svgGroup.append("g").attr("class","programAnnotation");
+        this.table = d3.select(_div).append('table').attr('id','interactiveMatrixTable')
         return
     };
 
@@ -42,8 +43,45 @@ class DataEntryMatrix{
         //////////////////////////////////////////////////////////////////////////////////
         "///////ANY GRAPHIC SPECIFIC DATA SHAPING SHOULD HAPPEN HERE////////"
         //////////////////////////////////////////////////////////////////////////////////
+        var rows = []
+        var headers = [
+            "","Standard","Good","Better","Living Community Principles","Regenerative"
+        ]
+        var headerRow = {
+            'text':headers,
+            'class':'matrixHeaderRow'
+        }
+        rows.push(headerRow)
+        var categoriesAddedToRows = []
+        for (var i of _data){
+            if (categoriesAddedToRows.includes(i['category']) != true){
+                categoriesAddedToRows.push(i['category'])
+                var rowText = [
+                    i['category'],'','','','',''
+                ]
+                var row = {
+                    'text':rowText,
+                    'class':'matrixCategoryHeaderRow'
+                }
+                rows.push(row)
+            }
+            var rowText = []
+            rowText.push(i['tag'])
+            for (var j of i['data']){
+                rowText.push(j['text'])
+            }
+            var row = {
+                'text':rowText,
+                'class':'matrixRow'
+            }
+            rows.push(row)
 
-        return _data
+        }
+        var builtData = {
+            'rowData':rows,
+            'rawData':_data
+        }
+        return builtData
 
     };
 
@@ -74,15 +112,28 @@ class DataEntryMatrix{
         "///////THIS FUNCTION DOES THE HEAVY LIFTING FOR CREATING GRAPHICS////////"
         //////////////////////////////////////////////////////////////////////////////////
         var _thisClass = this
-        var _data = this.data
+        var _rowData = this.data['rowData']
+        var _rawData = this.data['rawData']
         var _height = this.height
         var _width = this.width
         var _centerX = this.centerX
         var _centerY = this.centerY
 
         var _div = this.container
-        var _svg = this.svg
-        var _svgGroup = this.svgGroup
+        // var _svg = this.svg
+        // var _svgGroup = this.svgGroup
+        var _table = this.table
         
+        var updateRows = _table.selectAll('tr').data(_rowData).enter()
+            .append('tr')
+            .attr('class',function(d){
+                return d['class']
+            })
+            .selectAll('td').data(function(d,i){return d['text']}).enter()
+            .append('td')
+            .text(function(d){return d})
+    
+        
+        // console.log("drawing and updating")
     };
 };
