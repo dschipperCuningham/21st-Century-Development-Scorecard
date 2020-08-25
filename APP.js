1e8e6eb10866f9875faa4a1a39806d9ace85a9b3
Dustin Schipper <dschipper@cuningham.com>
2020-08-24 23:37:51 -0500

    about to make major changes switching to an Observer Design Pattern

--- APP.js
+++ APP.js
@@ -25,5 +25,20 @@ window.onresize = function(){
     resizeMatrix()
 }
 
+const config = { attributes: true, childList: true, subtree: true }
 
+const callback = function(mutationsList, observer) {
+    for (let mutation of mutationsList){
+        if (mutation.type === 'childList'){
+            console.log('A child node has been added or removed.');
+        }
+        else if (mutation.type === 'attributes'){
+            console.log('The ' + mutation.attributeName + ' attribute was modified');
+        }
+    }
+};
+
+const observer = new MutationObserver(callback)
+
+observer.observe(matrixCanvas, config)
 
