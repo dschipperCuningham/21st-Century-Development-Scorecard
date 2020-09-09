7fc39e0510050438fc1a4554af84ea5d9921d946
Dustin Schipper <dschipper@cuningham.com>
2020-09-08 22:48:04 -0500

    added axes to the line chart

--- js/lineChart.js
+++ js/lineChart.js
@@ -56,18 +56,15 @@ class LineChart{
         "///////GRAPHICS CONTROLS////////"
         //////////////////////////////////////////////////////////////////////////////////////
 
-
-        this.yBorder = 20
-        this.yOffset = 30
-        this.yMultiplier = (this.height - this.yBorder * 2 - this.yOffset) / 4
-
-        this.xBorder = 20
-        this.xOffset = 50
-        this.xMultiplier = (this.width -this.xBorder * 2 - this.xOffset) / (this.matrixData.length - 1)
-        for (let index in this.matrixData){
-            let datum = this.matrixData[index]
-            datum['x'] = index * this.xMultiplier + this.xBorder + this.xOffset
+        this.calculateDimensions();
+        let yAxisData = []
+        for (let i in this.matrixData[0]['data']){
+            let d = this.matrixData[0]['data'][i]
+            if (i != 0){
+                yAxisData.push(d)
+            }
         }
+        this.yAxisData = yAxisData
         console.log('matrix data')
         console.log(_model.matrixData)
 
@@ -83,6 +80,20 @@ class LineChart{
         return structuredData
     }
 
+    calculateDimensions(){
+        this.yBorder = 40
+        this.yOffset = 100
+        this.yMultiplier = (this.height - this.yBorder * 2 - this.yOffset) / 4
+
+        this.xBorder = 20
+        this.xOffset = 200
+        this.xMultiplier = (this.width -this.xBorder * 2 - this.xOffset) / (this.matrixData.length - 1)
+        for (let index in this.matrixData){
+            let datum = this.matrixData[index]
+            datum['x'] = index * this.xMultiplier + this.xBorder + this.xOffset
+        }
+    }
+
     resize(_model,_width,_height){
         this.width = _width;
         this.height = _height;
@@ -91,7 +102,11 @@ class LineChart{
         this.svg
             .attr('width',this.width)
             .attr('height',this.height)
+
+        this.calculateDimensions();
         this.drawUpdate(_model);
+
+        
         return
     }
 
@@ -116,6 +131,7 @@ class LineChart{
         var _precedentProjects = this.precedentProjects
         var _categoriesData = this.categoriesData
         var _matrixData = this.matrixData
+        var _yAxisData = this.yAxisData
 
         var _height = this.height
         var _width = this.width
@@ -138,13 +154,14 @@ class LineChart{
         var _yAxisText = this.yAxisText
 
         ////////D3 COMPLETE GENERAL UPDATE PATTERN///////// 
-        '///////CONTAINERS TO SEPARATE HEADERS FROM MATRIX////////'
+        '///////LINES FOR CHART////////'
 
         console.log('updated line chart')
         let updateLines = _linesGroup.selectAll('.chartLine').data(_data)
         let enterLines = updateLines.enter().append('path').attr('class','chartLine')
+            .attr('fill-opacity',0)
         let exitLines = updateLines.exit()
-        let mergeLines = enterLines.merge(updateLines)
+        let mergeLines = enterLines.merge(updateLines).transition()
             .style('stroke',function(d){
                 if (d['name'] == _projectName){
                     return '#fbec40'
@@ -155,8 +172,6 @@ class LineChart{
             .attr('d',function(d){
                 let coords = []
                 for (let datum of _matrixData){
-                    console.log(d)
-                    console.log(datum)
                     let coord = []
                     coord.push(datum['x'])
                     if (datum['tag'] in d['scores']){
@@ -164,21 +179,27 @@ class LineChart{
                     } else {
                         var scoreVal = 0
                     }
-                    coord.push((scoreVal * yMult) + yOffset + yBorder)
+                    coord.push(_height - ((scoreVal * yMult) + yOffset + yBorder))
                     coords.push(coord)
                 }
                 coords.push([
                     _width -  xBorder,
-                    yBorder + yOffset
+                    _height - (yBorder + yOffset)
                 ])
                 coords.push([
                     xBorder + xOffset,
-                    yBorder + yOffset
+                    _height - (yBorder + yOffset)
                 ])
                 let _line = d3.line().curve(d3.curveMonotoneX)(coords)
                 return _line
             })
-            .style('fill-opacity',function(d){ return 0.2})
+            .style('fill-opacity',function(d){
+                if (d['name'] == _projectName){
+                    return 1
+                } else {
+                    return 0.1
+                }
+            })
             .style('fill',function(d){
                 if (d['name'] == _projectName){
                     return '#fbec40'
@@ -186,18 +207,24 @@ class LineChart{
                     return 'gray'
                 }
             })
-
+        
+        ////////D3 COMPLETE GENERAL UPDATE PATTERN///////// 
+        '///////LINE POINTS////////'
         let updateLinePtGroups = _linesGroup.selectAll('.circleGroup').data(_data)
         let enterLinePtGroups = updateLinePtGroups.enter().append('g').attr('class','circleGroup')
         let exitLinePtGroups = updateLinePtGroups.exit().remove()
         let mergeLinePtGroups = enterLinePtGroups.merge(updateLinePtGroups)
 
         let updateLinePts = mergeLinePtGroups.selectAll('.linePt').data(function(d){
-            return objToArray(d['scores'])
+            let scoreArray = objToArray(d['scores'])
+            for (let datum of scoreArray){
+                datum['name'] = d['name']
+            }
+            return scoreArray
         })
-        let enterLinePts = updateLinePts.enter().append('circle').attr('class','linePt')
+        let enterLinePts = updateLinePts.enter().append('circle').attr('class','linePt').attr('fill','none')
         let exitLinePts = updateLinePts.exit().remove()
-        let mergeLinePts = enterLinePts.merge(updateLinePts)
+        let mergeLinePts = enterLinePts.merge(updateLinePts).transition()
             .attr('cx',function(d,i){
                 for (let datum of _matrixData){
                     if (datum['tag'] == d['key']){
@@ -207,8 +234,73 @@ class LineChart{
                 return _matrixData
             })
             .attr('cy',function(d){
-                return (d['value'] * yMult) + yOffset + yBorder
+                return _height - ((d['value'] * yMult) + yOffset + yBorder)
             })
-            .attr('r',3)
+            .attr('r',function(d){
+                if (d['name'] == _projectName){
+                    return 6
+                } else {
+                    return 3
+                }
+            })
+            .attr('stroke',function(d){
+                if (d['name'] == _projectName){
+                    return 'black'
+                } else {
+                    return 'none'
+                }
+            })
+            .attr('stroke-width',function(d){
+                if (d['name'] == _projectName){
+                    return '1px'
+                } else {
+                    return 'none'
+                }
+            })
+            .attr('fill',function(d){
+                if (d['name'] == _projectName){
+                    return '#fbec40'
+                } else {
+                    return 'black'
+                }
+            })
+        
+        ////////D3 COMPLETE GENERAL UPDATE PATTERN///////// 
+        '///////Y AXIS TEXT////////'
+        let updateYText = _yAxisText.selectAll('text').data(_yAxisData)
+        let enterYText = updateYText.enter().append('text').attr('class','yAxisText')
+        let exitYText = enterYText.exit().remove()
+        let mergeYText = enterYText.merge(updateYText).transition()
+            .text(function(d){
+                return d['score']
+            })
+            .attr('y',function(d){
+                return _height - ((d['value'] * yMult) + yOffset + yBorder)
+            })
+            .attr('x',function(d){
+                return xOffset
+            })
+            .attr('text-anchor','end')
+            .style('font-weight',750)
+        
+        ////////D3 COMPLETE GENERAL UPDATE PATTERN///////// 
+        '///////X AXIS DATA////////'
+        let updateXText = _xAxisText.selectAll('text').data(_matrixData)
+        let enterXText = updateXText.enter().append('text').attr('class','yAxisText')
+        let exitXText = enterXText.exit().remove()
+        let mergeXText = enterXText.merge(updateXText).transition()
+            .text(function(d){
+                return d['tag']
+            })
+            .attr('y',0)
+            .attr('x',0)
+            .attr('text-anchor','end')
+            .attr('transform',function(d){
+                let rotation = 320
+                let x = d['x']
+                let y = _height - yOffset - 15
+                return 'translate(' + x + ',' + y +') rotate(' + rotation + ')'
+            })
+            .style('font-weight',750)
     }
 }
